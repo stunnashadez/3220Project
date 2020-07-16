8a4ff54a5530305d4d2263fdf4053506b8ecf89a
Ravi Bhagat <bhagatr@uwindsor.ca>
2020-07-16 00:30:26 -0400

    Update DeleteExcel.java

--- DeleteExcel.java
+++ DeleteExcel.java
@@ -1,9 +1,7 @@
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
- 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -13,13 +11,14 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
  * Delete excel sheet
  */
 
-public class DeleteExcel {
- 
- 
-    public static void main(String[] args) {
+public class DeleteExcel 
+{
+     public static void main(String[] args) 
+     {
         String excelFilePath = "CommunityCentre.xlsx";
          
-        try {
+        try 
+        {
             FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
             Workbook workbook = WorkbookFactory.create(inputStream);
  
@@ -30,10 +29,10 @@ public class DeleteExcel {
             workbook.close();
             outputStream.close();
              
-        } catch (IOException | EncryptedDocumentException
-                | InvalidFormatException ex) {
+        } 
+        catch (IOException | EncryptedDocumentException | InvalidFormatException ex) 
+        {
             ex.printStackTrace();
         }
     }
- 
 }
